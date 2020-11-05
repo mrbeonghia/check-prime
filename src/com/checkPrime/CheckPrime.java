e7a64d2fcb34049c78b4f0acd84ae9ede45f0bca
mrbeonghia <ntnghia199x@gmail.com>
2020-11-05 09:48:08 +0700

    so nguyen to

--- src/com/checkPrime/CheckPrime.java
+++ src/com/checkPrime/CheckPrime.java
@@ -2,7 +2,7 @@ package com.checkPrime;
 
 import java.util.Scanner;
 
-public class Main {
+public class CheckPrime {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
